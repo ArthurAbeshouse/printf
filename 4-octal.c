27840b2749d4574be1189275891d92f200ecbfd0
eNobreg <ez@humdub.com>
2019-10-28 18:04:23 -0400

    added to header

---
+++ 4-octal.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+
+int print_octal(va_list n)
+{
+	unsigned int toOctal = va_arg(n, unsigned int);
+
+	int octalStore[48];
+	int i = 0, count = 0;
+	int j, oct;
+
+	while (toOctal != 0)
+	{
+		octalStore[i] = toOctal % 8;
+		toOctal = toOctal / 8;
+		i++;
+	}
+
+	for (j = i - 1 ; j >= 0 ; j--)
+	{
+		_putchar(octalStore[j]);
+		count++;
+	}
+}
