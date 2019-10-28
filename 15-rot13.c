c65186d18c0781287bde3afd14e6ec41ef6c5586
eNobreg <ez@humdub.com>
2019-10-28 12:41:00 -0400

    changes to accept a va_list

--- 15-rot13.c
+++ 15-rot13.c
@@ -1,21 +1,26 @@
 #include "holberton.h"
 
 
-void *rot13(char *str)
+void *rot13(const char *f, va_list str)
 {
+	char *stringTocheck;
+	va_start(str, str);
+
+	stringTocheck = va_arg(str, char*);
 	int i, j;
+	int count = 0;
 	char input[80] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 	char output[80] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
 
-	for (i = 0; str[i] != '\0'; i++)
+	for (i = 0; stringTocheck[i] != '\0'; i++)
 		for (j = 0; input[j] != '\0'; j++)
-			if (str[i] == input[j])
+			if (stringTocheck[i] == input[j])
 			{
-				str[i] = output[j];
-				_putchar(str[i]);
-				break;
-			
+				stringTocheck[i] = output[j];
+				_putchar(stringTocheck[i]);
+				count++;
+				break;	
 			}
 
 }
