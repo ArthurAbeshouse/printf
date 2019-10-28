6ae8c1013f1c39835d27ef07be67dd41c836432b
eNobreg <ez@humdub.com>
2019-10-28 18:04:51 -0400

    added to header

--- holberton.h
+++ holberton.h
@@ -14,7 +14,7 @@ typedef struct format
 
 int _putchar(char c);
 int (*get_spec_func(char speec))(va_list);
-int print_number(int args);
+int print_integer(va_list args);
 int print_char(va_list c);
 int print_binary(va_list bi);
 int print_string(va_list s);
@@ -24,5 +24,6 @@ void _puts(char *str);
 int print_lil_hex(va_list arg);
 int print_big_hex(va_list arg);
 int print_reverse(va_list n);
+int print_octal(va_list n);
 
 #endif
