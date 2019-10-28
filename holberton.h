10ac789a7201a94f41f2da72bad09ac0b09314e3
ArthurAbeshouse <31039144+ArthurAbeshouse@users.noreply.github.com>
2019-10-28 10:35:47 -0400

    updated header, created print integer/decimal and get spec

--- holberton.h
+++ holberton.h
@@ -6,7 +6,14 @@
 #include <stdarg.h>
 #include <unistd.h>
 
+typedef struct format
+{
+	char *op;
+	int (*f)(va_list);
+} spec_t;
 
 int _putchar(char c);
+int (*get_spec_func(char spec))(va_list)
+int print_number(int args)
 
 #endif
